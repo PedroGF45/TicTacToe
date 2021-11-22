93ea304ce0f4c60162c1afd1cf761a2ee0bc7fa5
Pedro Brito <pedrobfh@gmail.com>
2021-11-22 20:27:16 +0000

    Setup of request and responses for server

--- server.js
+++ server.js
@@ -4,5 +4,13 @@ const express = require("express");
 //get the app to run the function
 const app = express();
 
+//create users to store the information. Should store this in a database
+const users = [];
+
+
+app.get("/users", (req, res) => {
+    res.json(users)
+});
+
 //run the app on port 3000
 app.listen(3000);
