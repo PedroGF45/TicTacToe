66faf1538fdb005c4d35e272af34879f8874f326
Pedro Brito <pedrobfh@gmail.com>
2021-11-30 14:24:16 +0000

    Added back to login feature on password reset and account create

--- scripts.js
+++ scripts.js
@@ -34,18 +34,39 @@ function getPlayers () {
 const loginForm = document.getElementById("login");
 const passwordResetForm = document.getElementById("passwordReset");
 const createAccountForm = document.getElementById("createAccount");
-const getPasswordResetLink = document.getElementById("passwordResetLink");
-const getCreateAccountLink = document.getElementById("createAccountLink");
+const passwordResetLink = document.getElementById("passwordResetLink");
+const createAccountLink = document.getElementById("createAccountLink");
+const backToLoginPassword = document.getElementById("backToLoginPassword");
+const backToLoginAccount = document.getElementById("backToLoginAccount");
 
-getPasswordResetLink.addEventListener("click", function showPasswordForm() {
+passwordResetLink.addEventListener("click", function showPasswordForm() {
     loginForm.style.cssText = "visibility: hidden";
     createAccountForm.style.cssText = "visibility: hidden";
     passwordResetForm.style.cssText = "visibility: visible";
+    backToLoginPassword.style.cssText = "visibility: visible"; 
+    backToLoginAccount.style.cssText = "visibility: hidden";
 });
 
-getCreateAccountLink.addEventListener("click", function showCreateAccountForm() {
+createAccountLink.addEventListener("click", function showCreateAccountForm() {
     loginForm.style.cssText = "visibility: hidden";
     createAccountForm.style.cssText = "visibility: visible";
     passwordResetForm.style.cssText = "visibility: hidden";
+    backToLoginPassword.style.cssText = "visibility: hidden"; 
+    backToLoginAccount.style.cssText = "visibility: visible";
 });
 
+backToLoginPassword.addEventListener("click", function showLoginForm() {
+    loginForm.style.cssText = "visibility: visible";
+    createAccountForm.style.cssText = "visibility: hidden";
+    passwordResetForm.style.cssText = "visibility: hidden";
+    backToLoginPassword.style.cssText = "visibility: hidden"; 
+    backToLoginAccount.style.cssText = "visibility: hidden";
+});
+
+backToLoginAccount.addEventListener("click", function showLoginForm() {
+    loginForm.style.cssText = "visibility: visible";
+    createAccountForm.style.cssText = "visibility: hidden";
+    passwordResetForm.style.cssText = "visibility: hidden";
+    backToLoginPassword.style.cssText = "visibility: hidden"; 
+    backToLoginAccount.style.cssText = "visibility: hidden";
+});
