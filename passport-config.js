3dc54f3da9d2e842341014276026164ecf39eeb1
Pedro Brito <pedrobfh@gmail.com>
2021-12-04 16:12:36 +0000

    created app for checking if user and password are correct/incorrect

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt"); //to check if password is correct
+
+function initialize(passport, getUserByEmail) {
+    const authenticateUser = (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (user == null) {
+            return done(null, false, {message: "no user found"}); //if there's no user return null
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                //password is correct
+                return done(null, user)
+            } else {
+                //password is incorrect
+                return done(null, false, { message: "incorrect password"});
+            }
+        } catch (e) {
+            return done(e); //if happens any error show it
+        }
+    }
+
+    passport.use(new LocalStrategy({ usernameField: "email" }), authenticateUser)
+    passport.serializeUser((user, done) => {});
+    passport.deserializeUser((user, done) => {});
+}
+
+module.exports = initialize;
