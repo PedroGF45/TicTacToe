d0a27fc255f1bd26f7a4d763822a9a0a69311691
Pedro Brito <pedrobfh@gmail.com>
2021-12-04 15:52:06 +0000

    Added hashed password functionality

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express(); //get the app to run the function
 //setup and pull bcrypt library
 const bcrypt = require("bcrypt");
 
-//app accepts and use json
-app.use(express.json())
+//take the form and abble to get variables on form
+app.use(express.urlencoded({ extended: false}));
 
 
 //create users to store the information. Should store this in a database
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.post("/login", (req,res) => {
+
+});
+
 app.get("/login", (req, res) => {
     res.render("login.ejs");
 });
@@ -25,9 +29,21 @@ app.get("/register", (req, res) => {
     res.render("register.ejs");
 });
 
+app.post("/register", async (req,res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    } catch {
+
+    }
+});
+
 app.get("/recover", (req,res) => {
     res.render("recover.ejs");
-})
+});
+
+app.post("/recover", (req,res) => {
+
+});
 
 //run the app on port 3000
 app.listen(3000);
