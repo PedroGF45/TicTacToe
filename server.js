04185a2f15a9a7f9a07f2362b275c24494f17c10
Pedro Brito <pedrobfh@gmail.com>
2021-12-04 15:16:22 +0000

    Added root files for views of ejs library

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-//setup and pull express library
-const express = require("express"); 
-
-//get the app to run the function
-const app = express();
+const express = require("express"); //setup and pull express library
+const app = express(); //get the app to run the function
 
 //setup and pull bcrypt library
 const bcrypt = require("bcrypt");
@@ -14,54 +11,20 @@ app.use(express.json())
 //create users to store the information. Should store this in a database
 const users = [];
 
-app.get("/users", (req, res) => {
-    res.json(users)
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
 });
 
-app.post("/users", async (req, res) => {
-    try {
-        //generates a random hashed salt
-        const salt = await bcrypt.genSalt();
-        
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        
-        console.log(salt);
-        console.log(hashedPassword);
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
 
-        //requests name and password
-        const user = {
-            name: req.body.name,
-            password: hashedPassword
-        }
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
 
-         //pushes the user to the array of users
-        users.push(user);
-
-        //sends a response
-        res.status(201).send();
-    } catch {
-        res.status(500).send(); //in case something wrong
-    }
-})
-
-//login procedures, check if name and passwords are equal to the user created
-app.post("/users/login", async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    
-    //check if user exists
-    if (user == null) {
-        return res.status(400).send("User doesn't exist")
-    }
-    try {
-        if ( await bcrypt.compare(req.body.password, user.password)) { //check if hash is equal
-            res.send("Sucess");
-        } else {
-            res.send("Not Allowed");
-        }
-    } catch {
-        res.status(500).send();
-    }
-})
 
 //run the app on port 3000
 app.listen(3000);
